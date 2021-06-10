362f357fbb5418d1bb233b318a0f513c96c38f64
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-10 22:18:32 +0530

    Facebook CreatePost Option BottomSheet Example added to git. This is an Android Project that shows the use of the bottom sheet in android using the example of Facebook CreatePost Options. This project is developed as an example, the exact coding behind the Facebook CreatePost Options BottomSheet might be or is different.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.differentandroidcodes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void BottomSheet() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this, R.style.BottomSheetTheme);
+
+        View sheetView = LayoutInflater.from(MainActivity.this).inflate(R.layout.bottomsheet, (ViewGroup) findViewById(R.id.CreateNew));
+
+        bottomSheetDialog.setContentView(sheetView);
+        bottomSheetDialog.show();
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+        return true;
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.dashboard) {
+            BottomSheet();
+        }
+        return true;
+    }
+
+
+
+}
